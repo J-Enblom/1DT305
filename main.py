6ce67d473e0cd40fc41ba76717830e0852ca8d13
John <enblom2@gmail.com>
2023-07-04 17:48:00 +0200

    LED done

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import machine
+from picozero import LED
 import utime
 
 adc = machine.ADC(27)
+green_LED = LED(2)
+yellow_LED = LED(5)
+red_LED = LED(9)
+
 sf = 4095/65535 # Scale factor
 volt_per_adc = (3.3 / 4095)
-
 while True:
-    utime.sleep(2)
     millivolts = adc.read_u16()
 
     adc_12b = millivolts * sf
@@ -19,5 +22,16 @@ while True:
 
     shift = volt - 0.5
 
-    temp = shift / (dy / dx)
+    temp = round(shift / (dy / dx), 1) - 1 # for correct values
     print (temp)
+
+    if temp >= 18.0 and temp <= 22.0:
+        green_LED.blink(on_time=1,off_time=9, n=3, wait=True)
+        print("finished blinking")
+    elif temp >= 16.0 and  temp < 18.0 or temp > 22.0 and temp <= 24.0:
+        yellow_LED.blink(on_time=1,off_time=9, n=3, wait=True)
+        print("finished blinking")
+    else:
+        red_LED.blink(on_time=1,off_time=9, n=3, wait=True)
+        print("finished blinking")
+    
